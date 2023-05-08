96b60cdceb160f0a53a91f68267f303bb32f567d
shouldsee <shouldsee@github.com>
2023-05-09 05:29:25 +0800

    v3 cleaned and commented

--- main_zip.py
+++ main_zip.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# ##### Copyright 2023
+__doc__= '''check def main() for parameters and cli 
+arguments/options
+##### Copyright 2023
+'''
 
 
 import os
@@ -74,10 +76,18 @@ _init_scale = lambda shape=(): tfp.util.TransformedVariable(
     bijector=tfb.Softplus())
 
 ### use very wide uniform distribution for uninformed prior
-PRIOR_DIST = DummyDistribution
+# PRIOR_DIST = DummyDistribution
 def _get_prior(val=None,val2=0,name=None):
+  '''
+  global trigger on how to deal with prior
+  '''
   # _distc = _get_prior
+  # _distc = tfd.Uniform
+
+  ### Use a dummyDistribution to disable prior 
   _distc = DummyDistribution
+
+
   if val is None or isinstance(val,(float,int)):
     return _distc(-INF,INF,name=name)
   else:
@@ -85,6 +95,9 @@ def _get_prior(val=None,val2=0,name=None):
     return _distc(-INF+zval,INF+zval,name=name)    
 
 def make_joint_distribution_coroutine(feats, orgcode, norg, nfeat, method ='poisson'):
+  '''
+  For each method, initialise 3 objects
+  '''
   nsample = feats.shape[0]
   if method == 'poisson':
 
@@ -385,26 +398,6 @@ def make_joint_distribution_coroutine(feats, orgcode, norg, nfeat, method ='pois
     def get_summary(post):
       xd,xsample = post.sample_distributions()
       # k = 'log_zero_rate'
-      
-      # nmax = 5
-      # # xp 
-      # nh = 15
-      # xp = xd[k]
-      # for xpi,(xpmean,xpdev) in enumerate(zip(xp.mean(),xp.stddev())):
-      #   # xd[k]):
-      #   header = f'{k}.{xpi}'
-      #   header = header[:nh] + max(0,15-len(header))*' '+' '
-      #   print(f'{header}    mean:{xpmean}   stddev:{xpdev}')
-
-      # for xpi,(xpmean,xpdev) in enumerate(zip(xp.mean(),xp.stddev())):
-      #   # xd[k]):
-      #   print(f'{k}.{xpi}.sigmoid  mean:{tf.sigmoid(xpmean)}   stddev:{xpdev}')
-      # # for xpi,xpp in enumerate(xd[k]):
-      # #   print(f'{k}.sigmoid.{xpi} {tf.sigmoid(xpp.mean())}   {(xpp.stddev())}')
-
-
-      # # print(f'{k}          mean:{xd[k].mean()[:nmax]}  stddev:{xd[k].stddev()[:nmax]} ')
-      # # print(f'{k}.sigmoid  mean:{tf.sigmoid(xd[k].mean()[:nmax])}  stddev:{xd[k].stddev()[:nmax]} ')
 
   else:
     raise RuntimeError('Method {method!r} not understood!')    
@@ -418,24 +411,40 @@ import sys
 
 import numpy as np
 def main():
+  argv = sys.argv
+  ### gradient step size
   # learning_rate = 1e-2
   learning_rate = 5e-3
+
+  ### whether to stop on nan
   test_nan = 0
+
+  ### number of gradient steps
   num_steps = 3000
   # num_steps = 3000
+
+  ### step interval to update visdom plots
   int_plot_vis = 100
+
+  ### sample taken to estimate ELBO and gradient
   # sample_size=600
   # sample_size=2
   sample_size=10
+  
+  ### load data
   (feats, orgcode, count),df = load_data()
   # df.head()
-  method = 'poisson'
-  # method = 'zipoisson_shared_zero_rate'
-  # method = 'zipoisson_zero_rate_by_org'
-  # # method = 'zipoisson_zero_rate_full'
-  # method = 'zipoisson_orgcode_only'
+  
+  ### whether to force recomputation 
+  force = '--force' in argv
+
+  ### set model recipe. see README.md for explanation
+  v = 'poisson'
+  # v = 'zipoisson_shared_zero_rate'
+  # v = 'zipoisson_zero_rate_by_org'
+  # v = 'zipoisson_zero_rate_full'
+  # v = 'zipoisson_orgcode_only'
   v = 'zipoisson_zero_rate_feat_only'
-  argv = sys.argv
   key = '--method'
   v = 'poisson'
   if key in argv:
@@ -444,7 +453,7 @@ def main():
 
   
   output_file = f'{method}.{num_steps}.pkl'
-  force = '--force' in argv
+
 
   def plot_vis(xdist,loss,output_file=output_file):
     from visdom import Visdom
@@ -493,10 +502,7 @@ def main():
   # n_cat = orgco
   norg = len(set(orgcode))
   if int_plot_vis>0:
-    # fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 4))
-    # ax1.hist(count,pd.np.arange(0,30))
-    # fig.show()
-
+    #### this is for plotting 
 
     from visdom import Visdom
     vis = Visdom()
@@ -510,12 +516,12 @@ Labels shape:  {data_labels.shape}
   ''')
 
   if os.path.exists(output_file) and not force:
+    ### skip computation if file already exists
     with open(output_file,'rb')  as f:
       lossvals, xpar, xdist = pickle.load(f)
   else:
 
-
-
+    ### initialise instance
     joint, param_initer, get_summary = make_joint_distribution_coroutine(
         features,orgcode,norg,nfeat,
         # features.floor.values, features.county_code.values, df.county.nunique(),
@@ -531,33 +537,17 @@ Labels shape:  {data_labels.shape}
       return joint.log_prob(**args, likelihood=data_labels)
 
 
-
-    # ### Specify surrogate posterior
-
-
-
-
-
-
+    ### initialise parameter and thus surrogate_posterior
     init_param = param_initer()
     post = surrogate_posterior = tfd.JointDistributionNamedAutoBatched(init_param)
     tf.keras.callbacks.TerminateOnNaN().set_params(post.parameters)
 
-    # ### Results
-
+    ### RMSprop is very stable
 
     # optimizer = tf.optimizers.Adam(learning_rate=1e-2)
     optimizer = tf.optimizers.RMSprop(learning_rate=learning_rate)
     # optimizer = tf.optimizers.RMSprop(learning_rate=5e-3)
 
-    def tracer(args):
-      loss,grads,vars = args
-      print(loss)
-      # return loss
-      return args
-    post = surrogate_posterior
-
-    get_summary(surrogate_posterior)
     def callback(qty, post=post):
       print(f'[iter]{qty.step},  loss:{qty.loss:.1f}')
       if int_plot_vis>0 and ((qty.step)%int_plot_vis)==0:
@@ -576,6 +566,9 @@ Labels shape:  {data_labels.shape}
             breakpoint()
         return qty.loss
 
+    ### print some summary 
+    get_summary(surrogate_posterior)
+
     losses = tfp.vi.fit_surrogate_posterior(
         target_log_prob_fn, 
         surrogate_posterior,
@@ -584,17 +577,13 @@ Labels shape:  {data_labels.shape}
         # num_steps=300, 
         # trace_fn= lambda qty:[print(f'[iter]{qty.step},  loss:{qty.loss:.1f}'),qty.loss][-1],
         trace_fn = callback,
-        # trace_fn= lambda qty:[print(f'[iter]{qty.step},  loss:{qty.loss:.1f}'),
-        #   callback(qty,post),
-        #   print(post.sample_distributions()[0]['log_zero_rate'].mean().numpy()[:]),
-        #   print(post.sample_distributions()[0]['log_zero_rate'].stddev().numpy()[:]),
-        #   qty.loss][-1],
         num_steps=num_steps, 
         seed=42,
         sample_size=sample_size)
 
     lossvals = losses.numpy()
     xd,xsample = post.sample_distributions()
+
     print(f'Final Likelihood: {lossvals[-1]}')
     get_summary(post)
     with open(output_file,'wb') as f: 
@@ -604,7 +593,6 @@ Labels shape:  {data_labels.shape}
     xpar = post.parameters
 
   plot_vis(xdist,lossvals[-1])
-    # plt.plot()
 
 
 
